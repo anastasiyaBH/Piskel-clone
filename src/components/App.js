ad3d403514e41b9f654ef95774af0cb3c7a58597
AnastasiyaBH <bhanastasia495@gmail.com>
2019-06-23 14:01:37 +0300

    feat: add Eraser, PaintBucket, ColorSelector elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import Canvas from './Canvas/Canvas';
 import Pen from './Tools/Pen';
+import Eraser from './Tools/Eraser';
+import ToolsWrapper from './containers/ToolsWrapper'
+import PaintBucket from './Tools/PaintBucket'
+import ColorSelector from './Tools/ColorSelector'
 
 export default class App {
   constructor () {
@@ -7,7 +11,17 @@ export default class App {
 
   start () {
     const canvas = new Canvas();
-    const pen = new Pen(canvas);
+
+    const toolsWrapper = new ToolsWrapper();
+
+    const pen = new Pen(canvas), eraser = new Eraser(canvas), paintBucket = new PaintBucket(canvas);
+    const colorS = new ColorSelector();
+
+    colorS.set();
     pen.set();
+    eraser.set();
+    paintBucket.set();
+
+    toolsWrapper.set();
   }
 }
