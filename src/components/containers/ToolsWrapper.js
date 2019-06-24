d9ca67e317d264dbfff7f0899d5bf16b9f1ac67d
AnastasiyaBH <bhanastasia495@gmail.com>
2019-06-24 16:29:46 +0300

    fix: replace li on ul

--- src/components/containers/ToolsWrapper.js
+++ src/components/containers/ToolsWrapper.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-console */
 export default class ToolsWrapper {
   constructor() {
-    this.toolsWrapper = document.createElement('li');
+    this.toolsWrapper = document.createElement('ul');
     this.toolsWrapper.className = 'tool-wrapper';
 
     const toolColumn = document.querySelector('.tool-column');
@@ -11,7 +10,6 @@ export default class ToolsWrapper {
   set () {
     this.toolsWrapper.onclick = (event) => {
 
-    console.log(event.target.classList);
     if(event.target.classList.contains('tool-wrapper__tool')) {
 
       document.querySelectorAll('.tool-wrapper__tool').forEach((value) => {
